910e2dd4597fb1607e3ef7ba23af0b771682963d
Jonas <jappels@users.noreply.github.com>
2020-06-08 14:22:36 +0200

    Update v,h-diagram.py

--- v,h-diagram.py
+++ v,h-diagram.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 from ISA_calculator import ISA
 from v_max_calculator import get_max_velocity
 
-h=0
+
 rho=ISA(h)[2]
 A=4
 e=0.8
@@ -20,7 +20,9 @@ S=11
 W=1100*9.81
 C_D_0=0.03
 
+
+h = arange(0,4000, 10000)
+
 data = (P_a_max, k, S, W, C_D_0)
 
 results2= get_max_velocity(h, data)
-print(results2)
