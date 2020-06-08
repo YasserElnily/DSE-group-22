cc9f0dd18a446d2e508c31428cf3fe010e4479ae
Jonas <jappels@users.noreply.github.com>
2020-06-08 11:26:55 +0200

    Update v-h-diagram.py

--- v-h-diagram.py
+++ v-h-diagram.py
@@ -5,3 +5,15 @@ Created on Mon Jun  8 11:12:47 2020
 @author: jappe
 """
 
+from numpy import *
+from matplotlib import pyplot as plt
+from ISA_calculator import ISA
+h=0
+rho=ISA[2]
+A=6
+e=0.8
+k=1/(pi()*A*e)
+
+def v(C_L, W, S, rho):
+    return sqrt((W*2)/(S*rho*C_L))
+def
