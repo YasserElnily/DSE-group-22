d55477e3b0cc910366c62e82739d1152352435a3
TimoVMaarschalk <61225734+TimoVMaarschalk@users.noreply.github.com>
2020-06-09 14:19:02 +0200

    Input data file
    In this file all inputs that are not depended on the tools that are being created. Feel free to add more variables that need to be set.

---
+++ Aircraft_constance_Data.py
@@ -0,0 +1,19 @@
+#Input file
+"""In this file all inputs that are not depended on the tools that are being created. 
+Feel free to add more variables that need to be set."""
+
+#Fuselage section
+
+#wing section
+airfoil     = 'airfoil.dat' #this is the dat file of the coordinates of the airfoil geometry 
+frontspar   = .15 #front spar at 15% of the chord.
+rearspar    = .60  #rear spar at 60% of the chord.
+chord       = 2 #m
+span        = 7 #m
+taper       = 0 
+
+#propulsion section
+
+#Balance section
+
+
