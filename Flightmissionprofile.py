97b0c4a3418ad0f39cbd93e5c4467fbba367a253
ThomasKeesom <thomas.keesom@gmail.com>
2020-06-12 17:23:25 +0200

    Added: Flight mission profile code

---
+++ Flightmissionprofile.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 11:54:55 2020
+
+@author: Thomas
+"""
+
+#imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Input parameters
+ROC = 1000 #[ft/min]
+Vcruise = 160 #[km/h]
+Hcruisemax = 10000 #[ft]
+Hcruisemin = 1000 #[ft]
+Endurance = 180 #[min]
+Divergeendurance = 45 #[min]
+Range = 300 #[km]
+"""
+#Cruise at alt.
+#climb
+time = [0]
+altitude = [0]
+
+#climb to cruise alt
+tclimb = Hcruisemax / ROC
+hcruise = 
+"""
+#cruise profiles
+timemax = [0, 10, 170, 180]
+timemin = [0, 1, 179, 180]
+altitudemax = [0, 3048, 3048, 0]
+altitudemin = [0, 304.8, 304.8, 0]
+
+#loiter times
+hloiter = 304.8
+tend = Endurance + Divergeendurance
+descenttime = tend - (hloiter / ROC)
+
+tloiter = [179, 224, tend]
+altitudeloiter = [304.8, 304.8, 0]
+
+#Example flight
+examplealts = [0, 1524, 1524, 600, 600, 0]
+timeexamp = [0, 20, 160, 175, 215, 225]
+
+#plotting results
+plt.title("Flight mission profile")
+plt.ylabel("Altitude [m]")
+plt.xlabel("Time [min]")
+plt.xlim(0, 225)
+plt.ylim(0, 3500)
+plt.plot(timemax, altitudemax, label = "Cruise at service ceiling")
+plt.plot(timemin, altitudemin, label = "Cruise at minimum altitude")
+plt.plot(tloiter, altitudeloiter, label = "Loitering and diverting")
+plt.plot(timeexamp, examplealts, label = "Example flight", linestyle='dashed')
+plt.legend()
+plt.grid()
+plt.show()
+
+
+
