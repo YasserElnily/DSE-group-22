70ffe71e094956c55d154ea42c24227ab1b6ca3a
EliseScheers <elise.scheers@telenet.be>
2020-06-18 09:33:08 +0200

    new CLmax and CLalpha

--- V,n-Diagram.py
+++ V,n-Diagram.py
@@ -9,10 +9,10 @@ from ISA_calculator import ISA
 h = 0#11887.2
 m_to = 775#kg
 print("Altitude = " + str(h) + "m")
-C_L_max_clean = 1.27#2.023
+C_L_max_clean = 1.3944#2.023
 S_ref = 10.50
 cruise_speed= 220 #km/h
-C_L_alpha_M0 = 4.06 #lift curve at mach zero (nothing moment related)
+C_L_alpha_M0 = 4.633 #lift curve at mach zero (nothing moment related)
 MAC = 1.5
 #VERY IMPORTANT COMMENT: one should check line 130 (N_k) for the maximum load factor at TO and 'line3' should be changed to correct formula
 
